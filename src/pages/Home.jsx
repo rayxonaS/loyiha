e04a5987370c105d06b5fcb17a7f195bc035b10a
rayxona <rayxona@gmail.com>
2025-04-11 10:02:37 +0500

    fix: update styling for Home component with background color and text alignment improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,16 @@ function Home() {
   const { data } = useCollection("recepies");
 
   return (
-    <div className="max-w-6xl mx-auto px-4 py-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className="max-w-6xl mx-auto px-4 py-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 bg-amber-50">
       {data &&
         data.map((r) => {
           return (
             <div
               key={r.id}
-              className="bg-white shadow-md rounded-2xl overflow-hidden hover:shadow-xl transition-shadow duration-300"
+              className=" bg-purple-100 text-white shadow-md rounded-2xl overflow-hidden hover:shadow-xl transition-shadow duration-300"
             >
               <div className="p-5">
-                <h2 className="text-xl font-semibold mb-2 text-gray-800">
+                <h2 className=" text-center text-2xl font-semibold mb-2 text-gray-800">
                   {r.title}
                 </h2>
                 {r.description && (
